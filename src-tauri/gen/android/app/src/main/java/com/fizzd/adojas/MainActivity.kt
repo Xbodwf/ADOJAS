cb9c7d2282fbc61206e58c1dfbbbe76078b00b3c
flutas-web <flutas@outlook.com>
2025-02-06 21:25:28 +0800

    Develop with Tauri(1/1)

---
+++ src-tauri/gen/android/app/src/main/java/com/fizzd/adojas/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.fizzd.adojas
+
+class MainActivity : TauriActivity()
---
+++ src-tauri/gen/android/build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    repositories {
+        maven { url=uri("https://maven.aliyun.com/nexus/content/groups/public/")}
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+    }
+}
+
+allprojects {
+    repositories {
+        maven { url=uri("https://maven.aliyun.com/nexus/content/groups/public/")}
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
+
